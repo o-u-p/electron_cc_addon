1014c1f0648fa117ae938cdc97caef84e212a666
o-u-p <758033884@qq.com>
2017-03-26 15:07:09 +0800

    sample test

---
+++ myjs/detect.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    // this initializes the dialog (and uses some common options that I do)
+   
+});
+
+$("#startdetect").click(function () {
+    var i=0;
+    $('#startdetect').after('<div class="progress progress-striped progress-success active"><div id="processbar" class="progress-bar" style="width: 0%;"></div></div>')
+    var processbar = document.getElementById("processbar");
+    var addon = require("hello");
+    addon.hh();
+    var id = setInterval(frame, 10);
+    function frame(){
+        if(i>=100){
+            clearInterval(id);
+        } else {
+            i++;
+            processbar.style.width = i + "%";
+        }
+    }
+});
---
+++ myjs/entry.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $("#scanfdialog").dialog({
+        autoOpen: false, modal: true, show: "blind", hide: "blind"
+    });
+});
+
+function ScanFile() {
+
+    var addon = require("hello");
+    addon.scanf();
+}
